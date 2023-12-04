58a757e2d64225fd6059e80a964701513d1675bf
Alexi <aleksandragrinic@gmail.com>
2023-12-04 14:56:51 +0400

    data_base_dating/db_methods.py added

--- data_base_dating/db_vk_dating.py
+++ data_base_dating/db_vk_dating.py
@@ -1,17 +1,11 @@
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
 import sqlalchemy as sq
 import sqlalchemy.orm
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
-from vk_api import VkApi
 
 
 DSN = 'postgresql://postgres:mivida1@localhost:5432/dating'
 engine = sq.create_engine(DSN)
 
-Session = sessionmaker(bind=engine)
-session = Session()
 
 Base = declarative_base()
 
@@ -20,7 +14,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = sq.Column(sq.Integer, primary_key=True)
-    vk_user_id = sq.Column(sq.Integer, unique=True)
+    vk_user_id = sq.Column(sq.Integer, unique=True, index=True)
     first_name = sq.Column(sq.String(50), nullable=False)
     last_name = sq.Column(sq.String(50))
     age = sq.Column(sq.Integer)
@@ -29,7 +23,6 @@ class User(Base):
     profile_link = sq.Column(sq.String(255))
 
     def __init__(self, id: int, vk_user_id: int, first_name: str, last_name: str, age: int, gender: int, city: dict, profile_link: str):
-        self.id = id
         self.vk_user_id = vk_user_id
         self.first_name = first_name
         self.last_name = last_name
@@ -40,72 +33,19 @@ class User(Base):
 
 
 
-def add_user(session, user_id, vk_user_id, first_name, last_name, age, gender, city, profile_link):
-    new_user = User(
-        id=user_id,
-        vk_user_id=vk_user_id,
-        first_name=first_name,
-        last_name=last_name,
-        age=age,
-        gender=gender,
-        city=city['title'],
-        profile_link=profile_link
-        )
-    session.add(new_user)
-    session.commit()
-
-
-def user_in_group(api, group_id, user_id):
-    result = api.groups.isMember(group_id=group_id, user_id=user_id)
-    return result.get('member', 0) == 1
-
-    # Функция для авторизации в VK
-
-
-def auth_vk_with_token(access_token, api_version='5.131'):
-    vk_session = VkApi(token=access_token)
-    return vk_session.get_api(), vk_session
-
-    # Функция для получения данных пользователя из VK
-
-
-def get_vk_user_info(api, user_id):
-    user_info = api.users.get(user_ids=user_id, fields=['id', 'first_name', 'last_name', 'bdate', 'city', 'sex'])[0]
-    return user_info
-
-    # Функция для заполнения базы данных данными из VK
-
-
-def fill_database_from_vk(session, api, vk_user_id, group_id):
-    # Проверяем, состоит ли пользователь в группе
-    if user_in_group(api, group_id, vk_user_id):
-        # Если пользователь состоит в группе, получаем его информацию
-        user_info = get_vk_user_info(api, vk_user_id)
-
-        # Извлекаем нужные данные из user_info
-        user_id = user_info['id']
-        first_name = user_info['first_name']
-        last_name = user_info['last_name']
-        age = 2023 - int(user_info.get('bdate', '2023-01-01').split('.')[-1])  # Пример расчета возраста
-        gender = user_info['sex']
-        city = {'title': user_info.get('city', {}).get('title', 'Unknown')}
-        profile_link = f'https://vk.com/id{user_id}'
-
-        # Добавляем пользователя в базу данных
-        add_user(session, user_id, vk_user_id, first_name, last_name, age, gender, city, profile_link)
-
 class Favorite(Base):
     __tablename__ = 'favorites'
 
     id = sq.Column(sq.Integer, primary_key=True)
-    top_photos = sq.Column(sq.String(400))
-    id_user = sq.Column(sq.Integer, sq.ForeignKey('users.id'), nullable=False)
+    added_user_id = sq.Column(sq.Integer, index=True)
+    id_user = sq.Column(sq.Integer, sq.ForeignKey('users.id'), nullable=False, index=True)
+
+    sq.UniqueConstraint('added_user_id', 'id_user', name='uq_favorite_user_added_user_id')
 
     user = sqlalchemy.orm.relationship('User', backref='favorites')
 
-    def __init__(self, id: int, top_photos: str, id_user: int):
-        self.id = id
-        self.top_photos = top_photos
+    def __init__(self, id: int, added_user_id: int, id_user: int):
+        self.added_user_id = added_user_id
         self.id_user = id_user
 
 
@@ -113,14 +53,15 @@ class Blacklist(Base):
     __tablename__ = 'blacklist'
 
     id = sq.Column(sq.Integer, primary_key=True)
-    top_photos = sq.Column(sq.String(400))
-    id_user = sq.Column(sq.Integer, sq.ForeignKey('users.id'), nullable=False)
+    added_user_id = sq.Column(sq.Integer,index=True)
+    id_user = sq.Column(sq.Integer, sq.ForeignKey('users.id'), nullable=False, index=True)
+
+    sq.UniqueConstraint('added_user_id', 'id_user', name='uq_blacklist_user_added_user_id')
 
     user = sqlalchemy.orm.relationship('User', backref='blacklist')
 
-    def __init__(self, id: int, top_photos: str, id_user: int):
-        self.id = id
-        self.top_photos = top_photos
+    def __init__(self, id: int, added_user_id: int, id_user: int):
+        self.added_user_id = added_user_id
         self.id_user = id_user
 
 
